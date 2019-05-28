55b047912eb718ea1889a43b0b3f8d7765eb348e
wujieer96 <895326733@qq.com>
2019-05-28 21:31:05 +0800

    send

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price=0;
+    if inputs[distance]<=2{
+	price=inputs[parkTime]*0.25+6;
+	price=Math.round(price);
+	};
+    else if inputs[distance]>2 && inputs[distance]<=8 {
+	price=inputs[parkTime]*0.25+6+(inputs[distance]-2)*0.8;
+	price=Math.round(price);
+	};
+    else if inputs[distance]>8{
+	price=inputs[parkTime]*0.25+10.8+(inputs[distance]-8)*1.2;
+	price=Math.round(price);
+	};
+    return price;
 };
